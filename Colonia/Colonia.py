9bb391872e588bec1ae56dd3ac5456453345e340
tutuwajib66 <45153458+tutuwajib66@users.noreply.github.com>
2019-11-21 21:25:24 -0300

    Testes de Funções Feitas para escolha

--- Colonia/Colonia.py
+++ Colonia/Colonia.py
@@ -15,7 +15,7 @@ class Ant:
         self.posJ=proximo[0]
         self.posI=proximo[1]
         self.Caminho.append([self.posJ,self.posI])
-        self.PosDispo.remove([self.posJ,self.posI])
+        self.PosDispo.clear()
            
 
     def find(self):
@@ -23,17 +23,47 @@ class Ant:
             return True 
          
     def caminhoLivre(self,matrix):
-        if(matrix[self.posI][self.posJ+1]==0):
+        if(matrix.PathMatrix[self.posI][self.posJ+1]==0):
             self.PosDispo.append([self.posJ+1,self.posI])
 
-        if(matrix[self.posI][self.posJ-1]==0):
+        if(matrix.PathMatrix[self.posI][self.posJ-1]==0):
             self.PosDispo.append([self.posJ-1,self.posI])
 
-        if(matrix[self.posI+1][self.posJ]==0):
+        if(matrix.PathMatrix[self.posI+1][self.posJ]==0):
             self.PosDispo.append([self.posJ,self.posI+1])
 
-        if(matrix[self.posI-1][self.posJ]==0):
+        if(matrix.PathMatrix[self.posI-1][self.posJ]==0):
             self.PosDispo.append([self.posJ,self.posI-1])
+
+    def fechado(self):
+        if (len(self.PosDispo)==0):
+            return True
+        else: 
+            return False
+                
+    def choose(self,mapa):
+        if(len(self.PosDispo)==1):
+            self.andar(self.PosDispo[0])
+        else:
+            feromonios=[]
+            sum=0
+            for pnt in self.PosDispo:
+                feromonios.append(mapa.FeromonMatrix[pnt[1]][pnt[0]])
+                sum+=mapa.FeromonMatrix[pnt[1]][pnt[0]]
+
+            val=random.randint(0,int(sum))
+            intervalo1=int(feromonios[0])
+            intervalo2=int(feromonios[1]) + int(feromonios[0])
+            
+            if(val in range(1,intervalo1)):
+                self.andar(self.PosDispo[0])
+       
+            elif(val in range(intervalo1, intervalo2 + 1 ) ):
+                self.andar(self.PosDispo[1])
+            else:
+                self.andar(self.PosDispo[-1])
+
+
 
     
 
@@ -68,6 +98,7 @@ class Labrinth:
             matrix2.clear()
         
         f.close()
+        
 
         for line in matrix1:
             for i in range(0,len(line),2):
@@ -91,37 +122,31 @@ class Labrinth:
                 if(j==1):
                     matrix2.append(0)
                 else:
-                    matrix2.append(random.random())
+                    matrix2.append(random.random()*100)
             aux=matrix2.copy()
             self.FeromonMatrix.append(aux)
             matrix2.clear()
 
-    def Atualizapath(self,caminho):
-            posI=caminho[1]
-            posJ=caminho[0]
+    def Atualizapath(self,Formiga):
+            posI=Formiga.Caminho[-1][1]
+            posJ=Formiga.Caminho[-1][0]
             self.PathMatrix[posI][posJ]=4
     
-    def CleanTrail(self,caminho):
-         for pnt in caminho:
+    def CleanTrail(self,Formiga):
+         for pnt in Formiga.Caminho:
             self.PathMatrix[pnt[1]][pnt[0]]= 0 
 
-    def AtualizaFerom(self,caminho):
+    def AtualizaFerom(self,Formiga):
         for l in self.FeromonMatrix:
             for j in range(len(l)):
                 if (l[j] > 0):
                     l[j]=l[j]*(1-self.EvapoRate)
         
-        for pnt in caminho:
+        for pnt in Formiga.Caminho:
             self.FeromonMatrix[pnt[1]][pnt[0]]= self.FeromonMatrix[pnt[1]][pnt[0]]*(1.0+self.FeromRate)
 
                         
-            
-    def chose(self,FORMIGA):
-        cordenadas=[]
-        sum=0
-        for pnt in FORMIGA.PosDispo:
-            cordenadas.append(self.FeromonMatrix[pnt[1]][pnt[0]])
-            sum+=self.FeromonMatrix[pnt[1]][pnt[0]]
+    
         
               
         
@@ -133,35 +158,35 @@ Colonia=[]
 for i in range(50):
     Colonia.append(Ant(mapa.pnt_init,mapa.pnt_end))
 
-Colonia[0].caminhoLivre(mapa.PathMatrix)
-Colonia[0].andar([2,1])
-mapa.Atualizapath(Colonia[0].Caminho[-1])
+Colonia[0].caminhoLivre(mapa)
+Colonia[0].choose(mapa)
+mapa.Atualizapath(Colonia[0])
 
-Colonia[0].caminhoLivre(mapa.PathMatrix)
-Colonia[0].andar([3,1])
-mapa.Atualizapath(Colonia[0].Caminho[-1])
+Colonia[0].caminhoLivre(mapa)
+Colonia[0].choose(mapa)
+mapa.Atualizapath(Colonia[0])
 
-Colonia[0].caminhoLivre(mapa.PathMatrix)
-Colonia[0].andar([3,2])
-mapa.Atualizapath(Colonia[0].Caminho[-1])
+Colonia[0].caminhoLivre(mapa)
+Colonia[0].choose(mapa)
+mapa.Atualizapath(Colonia[0])
 
-Colonia[0].caminhoLivre(mapa.PathMatrix)
-Colonia[0].andar([3,3])
-mapa.Atualizapath(Colonia[0].Caminho[-1])
+Colonia[0].caminhoLivre(mapa)
+Colonia[0].choose(mapa)
+mapa.Atualizapath(Colonia[0])
 
-Colonia[0].caminhoLivre(mapa.PathMatrix)
-Colonia[0].andar([4,3])
-mapa.Atualizapath(Colonia[0].Caminho[-1])
+Colonia[0].caminhoLivre(mapa)
+Colonia[0].choose(mapa)
+mapa.Atualizapath(Colonia[0])
 
-Colonia[0].caminhoLivre(mapa.PathMatrix)
-Colonia[0].andar([5,3])
-mapa.Atualizapath(Colonia[0].Caminho[-1])
+Colonia[0].caminhoLivre(mapa)
+Colonia[0].choose(mapa)
+mapa.Atualizapath(Colonia[0])
 
 
 
-mapa.AtualizaFerom(Colonia[0].Caminho)
+mapa.AtualizaFerom(Colonia[0])
 
-mapa.CleanTrail(Colonia[0].Caminho)
+mapa.CleanTrail(Colonia[0])
 
 print("Fim")
 #for k in Colonia:
